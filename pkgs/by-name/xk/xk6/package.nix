74aab9162d397200b5dad8c7ecbc48e78f3196ea
Iván Szkiba <szkiba@users.noreply.github.com>
2025-02-19 19:06:29 +0100

    Update pkgs/by-name/xk/xk6/package.nix
    Co-authored-by: nayeko <196556004+nayeko@users.noreply.github.com>

--- pkgs/by-name/xk/xk6/package.nix
+++ pkgs/by-name/xk/xk6/package.nix
@@ -13,7 +13,7 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "grafana";
     repo = pname;
-    rev = "v${version}";
+    tag = "v${version}";
     hash = "sha256-pxiZelbAEEtibNdpEuRplPFtJ61YicoiAnAs8oaX6IA=";
   };
 

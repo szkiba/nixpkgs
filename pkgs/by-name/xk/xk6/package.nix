5f76d23292f4bbc6c846f687a0aad15cb0512509
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-02-19 18:07:28 +0100

    xk6: init at 0.14.3

---
+++ pkgs/by-name/xk/xk6/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+}:
+
+buildGoModule rec {
+  pname = "xk6";
+  version = "0.14.3";
+
+  src = fetchFromGitHub {
+    owner = "grafana";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-pxiZelbAEEtibNdpEuRplPFtJ61YicoiAnAs8oaX6IA=";
+  };
+
+  subPackages = [ "./" ];
+
+  sourceRoot = "${src.name}/cmd/xk6";
+
+  vendorHash = null;
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  doInstallCheck = false;
+
+  meta = with lib; {
+    description = "Build k6 with extensions";
+    mainProgram = "xk6";
+    homepage = "https://k6.io/";
+    changelog = "https://github.com/grafana/xk6/releases/tag/v${version}";
+    license = licenses.asl20;
+    maintainers = with maintainers; [
+      szkiba
+    ];
+  };
+}
